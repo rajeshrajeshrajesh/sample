737d38cd0fd92f9933f861b6a531faeb8dacb3d7
AS-MAC-1261 <as-mac-1261@AS-MAC-1261s-MacBook-Air.local>
2026-03-18 18:05:43 +0530

    added transformation

--- script/1.py
+++ script/1.py
@@ -2,3 +2,10 @@ from pyspark import SparkContext
 sc=SparkContext()
 xyz=sc.parallelize([1,2,3,4,5,6,7,7,8,8,8,8,8])
 print(type(xyz))
+
+rdd = sc.textFile("data/titanic_data.csv")
+
+print(rdd.take(5)) 
+
+header = rdd.first()
+data = rdd.filter(lambda row: row != header)
